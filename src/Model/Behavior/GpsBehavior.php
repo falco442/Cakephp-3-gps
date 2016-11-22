7b66aedc6cd491e57cfd659ba8a9ab2962528456
falco442 <marcofra83@gmail.com>
2016-11-22 16:44:29 +0100

    Updated behavior, created exception

--- src/Model/Behavior/GpsBehavior.php
+++ src/Model/Behavior/GpsBehavior.php
@@ -6,12 +6,13 @@ use Cake\ORM\Behavior;
 use Cake\ORM\Query;
 use Cake\ORM\Entity;
 use Cake\Datasource\EntityInterface;
+use Gps\Exception\MissingCoordinateException;
+
 
 class GpsBehavior extends Behavior{
 
 	protected $_defaultConfig = [
         'radius' => 6373044.737,
-        'model'=>'Stores',
         'fields'=>[
         	'latitude'	=>'latitude',
         	'longitude'	=>'longitude'
@@ -19,6 +20,34 @@ class GpsBehavior extends Behavior{
     ];
 
 	public function findNear(Query $query, array $options){
+
+        if(!isset($options['latitude']))
+            throw new MissingCoordinateException("Latitude not present");
+        if(!isset($options['longitude']))
+            throw new MissingCoordinateException("Longitude not present");
+        if(!isset($options['radius']))
+            throw new MissingCoordinateException("Radius not present");
+
+        extract($this->config());
+
+        list($minLat,$maxLat) = $this->_getMinMax($options['latitude'],$options['radius']);
+        list($minLon,$maxLon) = $this->_getMinMax($options['longitude'],$options['radius']);
+
+        $query->where(function ($exp, $q) use($fields,$minLat,$maxLat,$minLon,$maxLon) {
+            $exp->between($fields['latitude'], $minLat, $maxLat);
+            $exp->between($fields['longitude'], $minLon, $maxLon);
+            return $exp;
+        });
+
+        $query->where([$fields['latitude'].' IS NOT'=>null]);
+        $query->where([$fields['longitude'].' IS NOT'=>null]);
+
 		return $query;
 	}
+
+    protected function _getMinMax($coordinate,$choosenRadius = 0){
+        extract($this->config());
+        $return[] = $coordinate - $choosenRadius / $radius;
+        $return[] = $coordinate + $choosenRadius / $radius;
+    }
 }
